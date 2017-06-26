757fc3141e7f66df4ced6b01498cff7f2a3cb731
krzysztofk <krzysztof.kicinger@zooplus.com>
2017-06-26 14:10:58 +0200

    Added handler on search bar input change

--- redux-simple-starter/src/components/search_bar.js
+++ redux-simple-starter/src/components/search_bar.js
@@ -1,14 +1,20 @@
 /**
  * Created by krzysztofk on 2017-06-26.
  */
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
+class SearchBar extends Component {
     render() {
         return (
-            <input />
+            <input onChange={this.onInputChange} />
         )
     }
+
+    onInputChange(event) {
+        // console.log("On Input Change event occurred");
+        console.log("Value: " + event.target.value);
+    }
+
 }
 
 export default SearchBar;
