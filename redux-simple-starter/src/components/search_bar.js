144340b1558153cd3700cbfa29b5f097f0891b53
krzysztofk <krzysztof.kicinger@zooplus.com>
2017-06-26 15:32:48 +0200

    SearchBar constructor created

--- redux-simple-starter/src/components/search_bar.js
+++ redux-simple-starter/src/components/search_bar.js
@@ -4,14 +4,23 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+    constructor(props)  {
+        super(props);
+        this.state = {
+            term: ''
+        };
+    }
     render() {
         return (
             <input onChange={this.onInputChange} />
+            // <input onChange={event => console.log(event.target.value)} />
         )
     }
 
     onInputChange(event) {
         // console.log("On Input Change event occurred");
+        console.log("Event: " + event);
         console.log("Value: " + event.target.value);
     }
 
