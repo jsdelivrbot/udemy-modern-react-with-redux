da83870a9fe5a5f26fa36b4505ed1e71075b5a87
krzysztofk <krzysztof.kicinger@zooplus.com>
2017-06-26 13:33:16 +0200

    Introduced youtube-api-search with key as env var, Changed line endings to LF

--- redux-simple-starter/webpack.config.js
+++ redux-simple-starter/webpack.config.js
@@ -1,26 +1,33 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015', 'stage-1']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env' : {
+                'YOUTUBE_API_KEY': JSON.stringify(process.env.YOUTUBE_API_KEY)
+            }
+        })
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
 };
