c267aa75a179cadb9a5f91cda6c42752f8fc9512
krzysztofk <krzysztof.kicinger@zooplus.com>
2017-06-26 14:03:22 +0200

    Changed SearchBar to class component

--- redux-simple-starter/src/components/search_bar.js
+++ redux-simple-starter/src/components/search_bar.js
@@ -3,8 +3,12 @@
  */
 import React from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <input />
+        )
+    }
+}
 
 export default SearchBar;
