ef35ea9050f5e35bd615304079ac0b9fffd00fa0
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-07-02 00:26:51 +0200

    Added a video list, video detail and video list item

--- redux-simple-starter/src/components/video_list_item.js
+++ redux-simple-starter/src/components/video_list_item.js
@@ -1,3 +1,30 @@
 /**
  * Created by krzysztofk on 2017-06-26.
  */
+import React from 'react';
+
+// ({ video }) <==> (props) && const video = props.video
+const VideoListItem = ({video}) => {
+
+    const imageUrl = video.snippet.thumbnails.default.url;
+    const title = video.snippet.title;
+
+    console.log(video);
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl} />
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">
+                        {title}
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -1,28 +1,48 @@
 /**
  * Created by krzysztofk on 2017-06-26.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 
-YTSearch({
-    key: API_KEY,
-    term: 'surfboards'
-}, function(data) {
-    console.log(data);
-});
 
-// 1. Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            videos: []
+        };
+        this.initializeVideos();
+    }
+
+    initializeVideos() {
+        return YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        }, (videos) => {
+            this.setState( { videos } );
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        )
+    }
+
+}
 
 // 2. Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(
