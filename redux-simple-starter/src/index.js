1cb4fba7b3f245c5c3cd82024fc2f9932dcd3fe0
krzysztofk <krzysztof.kicinger@zooplus.com>
2017-07-13 10:31:39 +0200

    Throatling application with loadash and debounce method

--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -7,6 +7,7 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
@@ -36,9 +37,15 @@ class App extends Component {
     }
 
     render() {
+
+        const videoSearch = _.debounce(
+            term => { this.videoSearch(term) },
+            300
+        );
+
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
