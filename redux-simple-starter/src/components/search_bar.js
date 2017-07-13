768cfb197affebe481f827b2b233e5c073a84208
krzysztofk <krzysztof.kicinger@zooplus.com>
2017-07-13 09:49:01 +0200

    Basic CSS styling added

--- redux-simple-starter/src/components/search_bar.js
+++ redux-simple-starter/src/components/search_bar.js
@@ -8,18 +8,16 @@ class SearchBar extends Component {
     constructor(props)  {
         super(props);
         this.state = {
-            term: 'Starting term'
+            term: ''
         };
         this.onInputChange = this.onInputChange.bind(this);
     }
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term}
                     onChange={event => this.setState({ term : event.target.value })} />
-                    {/*onChange={this.onInputChange} />*/}
-                Value of the input: { this.state.term }
             </div>
         )
     }
