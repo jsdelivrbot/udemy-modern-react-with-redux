a4ab6557c97e8c7a643fe73fbec7561557ccf968
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-07-14 18:30:11 +0200

    Added validate function

--- blog-app/src/components/posts_new.js
+++ blog-app/src/components/posts_new.js
@@ -40,6 +40,33 @@ class PostsNew extends Component {
 
 }
 
+// This function will be called during some parts of the lifecycle of the form
+// It is passed in config object to reduxFrom
+function validate(values) {
+    console.log("Values: " + JSON.stringify(values));
+    const errors = {};
+
+    // Validate the inputs from 'values'
+    if(!values.title) {
+        errors.title = "Enter a title!";
+    }
+
+    if(!values.categories) {
+        errors.categories = "Enter some categories!";
+    }
+
+    if(!values.content) {
+        errors.content = "Enter some content please!";
+    }
+
+
+
+    // If errors is empty, the form is fine to submit
+    // If errors has any properties, redux form assumes form is invalid
+    return errors;
+}
+
 export default reduxForm({
-    form: 'PostsNewForm' // Name of the form
+    form: 'PostsNewForm', // Name of the form
+    validate
 })(PostsNew);
