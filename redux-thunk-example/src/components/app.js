ff40fbe0fd57d7cfab1802719aa3afb6a3e70b29
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-07-14 23:25:10 +0200

    redux-thunk example

--- redux-thunk-example/src/components/app.js
+++ redux-thunk-example/src/components/app.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+class App extends Component {
+
+    componentWillMount() {
+        this.props.fetchUsers();
+    }
+
+    renderUser({
+        id,
+        name,
+        email
+    }) {
+        return (
+            <li key={id} className="list-group-item">
+                <span className="pull-xs-right">
+                    <a href={email}>{email}</a>
+                </span>
+                { name }
+            </li>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Email Directory</h4>
+                <ul className="list-group">
+                    { this.props.users.map(this.renderUser) }
+                </ul>
+            </div>
+        );
+    }
 }
+
+export default connect(state => state, actions)(App);
